1268db0c0a74d497a52c0f1c81b50acbfb99f2b1
notpr <notpr@DESKTOP>
2024-11-20 19:56:20 -0800

    forgot about this guy

--- classes/Chess.h
+++ classes/Chess.h
@@ -8,6 +8,13 @@ const int pieceSize = 64;
 const int White = 0;
 const int Black = 128;
 
+enum CastleStatus {
+  Q = 1,
+  K = 2,
+  q = 4,
+  k = 8,
+};
+
 enum ChessPiece {
   NoPiece = 0,
   Pawn = 1,
@@ -56,21 +63,34 @@ public:
   }
 
   std::vector<Move> getMoves() { return _moves; };
+  int getCS() { return _castle_status; };
+  int getEnpasIndex() { return _enpas_index; };
 
   void updateAI() override;
   bool gameHasAI() override { return true; }
+
+  std::string stateStringPretty();
 
 private:
   Bit *PieceForPlayer(const int playerNumber, ChessPiece piece);
   const char bitToPieceNotation(int row, int column) const;
   void setPieceAt(int rank, int file, int playerNumber, ChessPiece piece);
+  void setGameFromFEN(const std::string &string);
   void setBoardFromFEN(const std::string &string);
   void GenerateMoves(int playerNumber);
   void GenerateSlidingMoves(int StartSquare, int tag);
   void GenerateKnightMoves(int start_index, int tag);
   void GeneratePawnMoves(int start_index, int tag);
   void GenerateKingMoves(int start_index, int tag);
+  void GenerateCastlingMoves(int start_index, int tag);
+  void updateExtrinsicState(BitHolder &src, BitHolder &dst);
+  bool canCastle(bool is_white, bool kingside);
+  void disableCastlability(bool is_white, bool kingside);
+  void disableCastlability(bool is_white);
 
   ChessSquare _grid[8][8];
+  std::string _state;
   std::vector<Move> _moves;
+  int _castle_status = 15;
+  int _enpas_index = 64;
 };
